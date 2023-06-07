b097a15851c9ff239d8f46d2e3117694289b934f
Oscar Okumu <ookumu@songa.app>
2023-06-07 15:49:23 +0300

    updated file

--- lib/models.py
+++ lib/models.py
@@ -1,46 +1,32 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
-# from lib.models import Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+# Define the User table
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    email = Column(String(30))
+    password_hash = Column(String(30))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return str(self.id)
+
+# @login.user_loader
+# def load_user(id):
+#     return Users.query.get(int(id))
+    
+
 engine = create_engine('sqlite:///todo_test.db')
-# Base.metadata.create_all(engine)
 
-# # list
-# class Task(Base):
-#     __tablename__ = 'tasks'
-#     id = Column(Integer, primary_key=True)
-#     title = Column(String)
-#     user_id = Column(Integer, ForeignKey('users.id'))
-#     category_id = Column(Integer, ForeignKey('categories.id'))
-
-#     user = relationship('User', back_populates='tasks')
-#     category = relationship('Category', back_populates='tasks')
-
-# def get_tasks():
-#     tasks = Task.query.all()
-#     return tasks
-
-# def get_completed_tasks(tasks):
-#     completed_tasks = [task for task in tasks if task.completed]
-#     return completed_tasks
-
-# categories = ['Work', 'Personal', 'Shopping']
-
-
-# # dictionary
-# class user(Base):
-#     __tablename__ = 'users'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String)
-#     tasks = relationship('Task', back_populates='user')
-
-# # tuple
-# class Category(Base):
-#     __tablename__ = 'categories'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String)
-#     tasks = relationship('Task', back_populates='category')
+Base.metadata.create_all(engine)
